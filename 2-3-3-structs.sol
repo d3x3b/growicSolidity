bb5ac153320397ca6ac6168437e7ef64488d3457
d3x3d <jacco.van.de.berg@gmail.com>
2023-02-24 14:44:49 +0100

    added check to getUserDetails for existing account

--- 2-3-3-structs.sol
+++ 2-3-3-structs.sol
@@ -3,35 +3,40 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract structs {
-
-    struct user{
+    struct user {
         string name;
-        uint age;
+        uint256 age;
         bool registered;
     }
 
-mapping (address => uint) private balance;
-mapping (address => user) private userDetails;
+    mapping(address => uint256) private balance;
+    mapping(address => user) private userDetails;
 
-function setUserDetails(string memory _name, uint256 _age) public {
-require (!userDetails[msg.sender].registered, "User already KYC'ed.");    
-        userDetails[msg.sender].name=_name;
-        userDetails[msg.sender].age=_age;
-        userDetails[msg.sender].registered=true;
-}
+    function setUserDetails(string memory _name, uint256 _age) public {
+        require(!userDetails[msg.sender].registered, "User already KYC'ed.");
+        userDetails[msg.sender].name = _name;
+        userDetails[msg.sender].age = _age;
+        userDetails[msg.sender].registered = true;
+    }
 
-function getUserDetails () public view returns (string memory _name, uint _age) {
-    if (userDetails[msg.sender] == true{
-    return (_name = userDetails[msg.sender].name, _age = userDetails[msg.sender].age);
-}
-}
+    function getUserDetails()
+        public
+        view
+        returns (string memory _name, uint256 _age)
+    {
+        require(userDetails[msg.sender].registered, "User did not register");
+        return (
+            _name = userDetails[msg.sender].name,
+            _age = userDetails[msg.sender].age
+        );
+    }
 
-function deposit (uint _depositBalance) public {
-    require (_depositBalance >= 1, "Cannot deposit 0");
-    balance[msg.sender]=_depositBalance;
-}
+    function deposit(uint256 _depositBalance) public {
+        require(_depositBalance >= 1, "Cannot deposit 0");
+        balance[msg.sender] = _depositBalance;
+    }
 
-function checkBalance () public view returns (uint _balance){
-return (_balance = balance[msg.sender]);
-}
+    function checkBalance() public view returns (uint256 _balance) {
+        return (_balance = balance[msg.sender]);
+    }
 }
